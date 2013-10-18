e67f092dd17531f908e57f2ce549edb62e1ae8ce
weirdobeaver <weirdobeaver88@gmail.com>
2013-10-18 15:21:50 +0200

    Usernam added to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 	def create
-		comment = Comment.new(body: params[:comment][:body], post_id: params[:comment][:post_id])
+		comment = Comment.new(body: params[:comment][:body], 
+													post_id: params[:comment][:post_id],
+													user: current_user)
 	  comment.save
 	  redirect_to post_path(comment.post)
 	end
