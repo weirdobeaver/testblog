0f72e77f9eb35f2478a9222cbbd5f262c0de3fbf
weirdobeaver <weirdobeaver88@gmail.com>
2013-10-21 14:26:45 +0200

    Unmarking commit as abusie by post author added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,11 @@ class CommentsController < ApplicationController
 	  redirect_to post_path(comment.post)
 	end
 
+	def update
+		comment = Comment.find(params[:id])
+		comment.abusive = params[:comment][:abusive]
+		comment.save
+		redirect_to post_path(comment.post)
+	end
+
 end
